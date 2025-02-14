9f8fc3b295944f1e4b9e88f51166c830bd2ad73a
unknown <biazzioxbox@gmail.com>
2025-02-14 15:33:21 -0300

    aula 3 controlando o estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import "./reset.css";
 import Banner from "./componentes/Banner/index.jsx";
 import Formulario from "./componentes/Formulario/index.jsx"; 
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  
   return (
     <>
       <Banner />
--- src/componentes/CampoTexto/index.jsx
+++ src/componentes/CampoTexto/index.jsx
@@ -2,12 +2,19 @@ import './CampoTexto.css';
 
 const CampoTexto = (props) => {
 
+    let valor = "Bruno de Biazio"
+
     const placeholderModificada= `${props.placeholder}...`
+
+    const aoDigitado = (qualquercoisa) => {
+        valor= qualquercoisa.target.value
+        console.log(valor)
+    }
     
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
