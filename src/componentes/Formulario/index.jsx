43ced5357c7bf5e20ea843435a6b59eb0ca4dee3
unknown <biazzioxbox@gmail.com>
2025-02-25 21:41:08 -0300

    aula 4 finalizada -faltou limpar somente

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -5,15 +5,6 @@ import Botao from "../Botao";
 import { useState } from "react";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "DevOps",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
 
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
@@ -58,7 +49,7 @@ const Formulario = (props) => {
         <ListaSuspensa
           obrigatorio={true}
           label="Time"
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={(valor) => setTime(valor)}
         />
